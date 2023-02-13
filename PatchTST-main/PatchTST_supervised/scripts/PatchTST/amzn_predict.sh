d10cac1e822f5d947d5c033c094f25238bfa074c
chengyanwu <cheng-yan@umail.ucsb.edu>
2023-02-12 17:59:56 -0800

    add prediction sh file

--- PatchTST-main/PatchTST_supervised/scripts/PatchTST/amzn_predict.sh
+++ PatchTST-main/PatchTST_supervised/scripts/PatchTST/amzn_predict.sh
@@ -14,11 +14,11 @@ model_id_name=amazon
 data_name=custom
 
 random_seed=2021
-for pred_len in 7 14 28 54
+for pred_len in 7
 do
     python3 -u run_longExp.py \
       --random_seed $random_seed \
-      --is_training 1 \
+      --is_training 0 \
       --root_path $root_path_name \
       --data_path $data_path_name \
       --model_id $model_id_name_$seq_len'_'$pred_len \
@@ -38,7 +38,4 @@ do
       --patch_len 16\
       --stride 8\
       --des 'Exp' \
-      --train_epochs 100\
-      --patience 20\
-      --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
 done
